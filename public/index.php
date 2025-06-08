9772038f1d02d0b91ca2e93d800d50cb301c6439
fatmatawfeek <fatmatawfeek97@gmail.com>
2025-06-09 01:16:51 +0300

    allow CORS and add .sql file

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
 <?php
 
-use App\Models\Product;
-
 require_once __DIR__ . '/../vendor/autoload.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
 
 $dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
     $r->post('/graphql', [App\Controller\GraphQLController::class, 'handle']);
